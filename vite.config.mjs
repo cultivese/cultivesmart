929921260d72d0e546fb33af3798255cbe32c307
unknown <administrador>
2025-01-16 08:50:22 -0300

    bugfix: fix refresh page

--- vite.config.mjs
+++ vite.config.mjs
@@ -47,6 +47,7 @@ export default defineConfig(() => {
     },
     server: {
       port: 3000,
+      historyApiFallback: true,
       proxy: {
         // https://vitejs.dev/config/server-options.html
       },
